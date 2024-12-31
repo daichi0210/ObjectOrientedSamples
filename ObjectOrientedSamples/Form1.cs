fbbcaac23a3efc4cf66751e8dc4e5f7225d31e40
daichi <kabata0210@icloud.com>
2024-12-31 20:53:27 +0900

    完成

--- ObjectOrientedSamples/Form1.cs
+++ ObjectOrientedSamples/Form1.cs
@@ -6,5 +6,23 @@ namespace ObjectOrientedSamples
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.White;
+            label1.ForeColor = Color.Red;
+            label1.Text = "‚±‚ñ‚É‚¿‚Í";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Hide();
+            MessageBox.Show("‚±‚ñ‚É‚¿‚Í");
+        }
+
+        public void MyMethod()
+        {
+
+        }
     }
 }
--- ObjectOrientedSamples/obj/Debug/net6.0-windows/ObjectOrientedSamples.AssemblyInfo.cs
+++ ObjectOrientedSamples/obj/Debug/net6.0-windows/ObjectOrientedSamples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObjectOrientedSamples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc2fbe50f785c3ccfdcd7c5b90c56a1ccdb9263")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObjectOrientedSamples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObjectOrientedSamples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
